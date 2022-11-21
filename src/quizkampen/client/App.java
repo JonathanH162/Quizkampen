db2decfce0032f67f1f488f92a8986e3c915f521
Jonathan Hellgren <jonathan.hellgren7@gmail.com>
2022-11-21 15:08:27 +0100

    Start på en enkel GUI till poänglobbyn.

--- src/quizkampen/client/App.java
+++ src/quizkampen/client/App.java
@@ -2,39 +2,73 @@ package quizkampen.client;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class App extends JFrame {
+
+public class App extends JFrame implements ActionListener {
+    int player1RoundPoints = 0;
+    int player2RoundPoints = 0;
+    int player1TotalPoints = 0;
+    int player2TotalPoints = 0;
+    String category = "Category";
     JPanel mainPanel = new JPanel();
-    JPanel player1Panel = new JPanel();
-    JPanel player2Panel = new JPanel();
-    JLabel nameLabel1 = new JLabel("Spelare 1");
-    JLabel nameLabel2 = new JLabel("Spelare 2");
+    JPanel namePanel = new JPanel();
+    JPanel totalScorePanel = new JPanel();
+    JPanel pointPanel1 = new JPanel();
+    JPanel pointPanel2 = new JPanel();
+    JPanel pointPanel3 = new JPanel();
+    JPanel categoryAndPointPanel = new JPanel();
+    JLabel nameLabel1 = new JLabel("Player 1");
+    JLabel nameLabel2 = new JLabel("Player 2");
+    JLabel totalScoreLabel = new JLabel("Total Score", SwingConstants.CENTER);
     JButton playButton = new JButton("Next Round");
-    int player1Points = 0;
-    int player2Points = 0;
+    JLabel totalScorecounter1 = new JLabel(String.valueOf(player1TotalPoints));
+    JLabel totalScorecounter2 = new JLabel(String.valueOf(player2TotalPoints));
 
-    JLabel pointLabel1 = new JLabel(String.valueOf(player1Points), SwingConstants.CENTER);
-    JLabel pointLabel2 = new JLabel(String.valueOf(player2Points), SwingConstants.CENTER);
 
 
     App() {
         this.add(mainPanel);
         mainPanel.setLayout(new BorderLayout());
-        mainPanel.add(player1Panel, BorderLayout.EAST);
-        mainPanel.add(player2Panel, BorderLayout.WEST);
+        mainPanel.add(namePanel, BorderLayout.NORTH);
+        mainPanel.add(categoryAndPointPanel, BorderLayout.CENTER);
         mainPanel.add(playButton, BorderLayout.SOUTH);
-        player1Panel.setLayout(new GridLayout(2,1));
-        player2Panel.setLayout(new GridLayout(2,1));
-        player1Panel.add(nameLabel1);
-        player1Panel.add(pointLabel1);
-        player2Panel.add(nameLabel2);
-        player2Panel.add(pointLabel2);
+        playButton.addActionListener(this);
+        namePanel.setLayout(new BorderLayout());
+        pointPanel1.setLayout(new BorderLayout());
+        pointPanel2.setLayout(new BorderLayout());
+        pointPanel3.setLayout(new BorderLayout());
+        namePanel.add(nameLabel1, BorderLayout.WEST);
+        namePanel.add(nameLabel2, BorderLayout.EAST);
+        categoryAndPointPanel.setLayout(new GridLayout(4,1));
+        categoryAndPointPanel.add(addPointPanel(pointPanel1));
+        categoryAndPointPanel.add(addPointPanel(pointPanel2));
+        categoryAndPointPanel.add(addPointPanel(pointPanel3));
+        totalScorePanel.setLayout(new BorderLayout());
+        totalScorePanel.add(totalScoreLabel, BorderLayout.CENTER);
+        totalScorePanel.add(totalScorecounter1, BorderLayout.EAST);
+        totalScorePanel.add(totalScorecounter2, BorderLayout.WEST);
+        categoryAndPointPanel.add(totalScorePanel);
 
-
+        setTitle("Quizkampen");
         setSize(300,400);
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    public JPanel addPointPanel(JPanel pointPanel) {
+        pointPanel.add(new JLabel(String.valueOf(player1RoundPoints)),BorderLayout.WEST);
+        pointPanel.add(new JLabel(String.valueOf(player2RoundPoints)),BorderLayout.EAST);
+        pointPanel.add(new JLabel(category, SwingConstants.CENTER),BorderLayout.CENTER);
+        return pointPanel;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == playButton) {
+
+        }
     }
 
     public static class Main {
