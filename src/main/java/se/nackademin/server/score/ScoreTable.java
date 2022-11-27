7ea07029c765d7d4cb3db8d5fd45e5d7d4f7efde
BaharehTorki <bahareh.torki@yh.nackademin.se>
2022-11-27 02:02:22 +0100

    - Adding functionality for calculating the score. - Adding Test class

---
+++ src/main/java/se/nackademin/server/score/ScoreTable.java
@@ -0,0 +1,53 @@
+package se.nackademin.server.score;
+
+import se.nackademin.utils.ConfigProperty;
+
+public class ScoreTable {
+
+    private ConfigProperty property;
+    private boolean[][] score;
+
+    private static int row;
+    private static int column;
+
+    public ScoreTable(ConfigProperty property) {
+        row = 0;
+        column = 0;
+        this.property = property;
+        this.score = new boolean[this.property.getNumberOfRound()][this.property.getNumberOfQuestion()];
+    }
+
+    public void print() {
+        for (boolean[] row : score) {
+            for (boolean col : row) {
+                System.out.print(String.format("%5s", (col) ? 1 : 0));
+            }
+            System.out.println();
+        }
+    }
+
+    public void setScore(boolean result) {
+        score[row][column] = result;
+
+        if (column < property.getNumberOfQuestion() - 1) {
+            column++;
+        } else if (row < property.getNumberOfRound() - 1) {
+            row++;
+            column = 0;
+        } else {
+            throw new ArrayIndexOutOfBoundsException("You have put all result in score table");
+        }
+    }
+
+    public int calculate() {
+        int sumOfScore = 0;
+        for (int row = 0; row < property.getNumberOfRound(); row++) {
+            for (int col = 0; col < property.getNumberOfQuestion(); col++) {
+                if (score[row][col] == true) {
+                    ++sumOfScore;
+                }
+            }
+        }
+        return sumOfScore;
+    }
+}
---
+++ src/test/java/se/nackademin/server/score/ScoreTableTest.java
@@ -0,0 +1,81 @@
+package se.nackademin.server.score;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import se.nackademin.utils.ConfigProperty;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class ScoreTableTest {
+
+    ScoreTable sut;
+
+    @Mock
+    private ConfigProperty property;
+
+    @BeforeEach
+    void setUp() {
+        doReturn(3).when(property).getNumberOfRound();
+        doReturn(7).when(property).getNumberOfQuestion();
+    }
+
+    @Test
+    void should_print_correct_amount_of_property_values() {
+        sut = new ScoreTable(property);
+        sut.print();
+    }
+
+    @Test
+    void should_set_correctly() {
+        sut = new ScoreTable(property);
+        int tableCells = property.getNumberOfRound() * property.getNumberOfQuestion();
+
+        Random random = new Random();
+        for (int i = 0; i < tableCells-1; i++) {
+            sut.setScore(random.nextBoolean());
+        }
+        sut.print();
+    }
+
+    @Test
+    void throw_an_exception_if_setScore_calls_after_that_all_question_are_finished() {
+        sut = new ScoreTable(property);
+        int tableCells = property.getNumberOfRound() * property.getNumberOfQuestion();
+
+        Random random = new Random();
+        for (int i = 0; i < tableCells-1; i++) {
+            sut.setScore(random.nextBoolean());
+        }
+
+        assertThrows(ArrayIndexOutOfBoundsException.class,()-> sut.setScore(true));
+    }
+
+    @Test
+    void should_calculate_correct_total_score() {
+        //given
+        int expectedScore = 5;
+        sut = initialTable(expectedScore);
+        sut.print();
+
+        //When
+        int actualScore = sut.calculate();
+        //Then
+        assertEquals(expectedScore, actualScore);
+
+
+    }
+
+    private ScoreTable initialTable(int trueAmount) {
+        ScoreTable scoreTable = new ScoreTable(property);
+        for (int i = 0; i < trueAmount; i++) {
+            scoreTable.setScore(true);
+        }
+        return scoreTable;
+    }
+}
